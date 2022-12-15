c2ff74d9161c6fef9fd4c11c8b7f949ae0d7ea48
dessinger <108768144+Julian-Correa@users.noreply.github.com>
2022-12-15 17:13:10 -0300

    Create opb.js

---
+++ opb.js
@@ -0,0 +1,11 @@
+function sumar(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+function main() {
+  // Llamamos a la función sumar y le pasamos tres números
+  let resultado = sumar(1, 2, 3);
+  console.log(resultado); // debería mostrar 6
+}
+
+main();
